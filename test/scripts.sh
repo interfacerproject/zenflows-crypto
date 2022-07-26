88a7e1d40b5e6d56d8dd7ee88aa995e07f8595a5
Jaromil <jaromil@dyne.org>
2022-07-26 16:01:33 +0200

    fix keypairoom (still WIP)

--- test/scripts.sh
+++ test/scripts.sh
@@ -27,6 +27,26 @@ testzen sign ${v_gqlsigned} ${gqljson} ${keyring} > ${gqlsigned}
 ## SERVER
 testzen verify_graphql '{"output":["VALID_SIGNATURE"]}' ${gqlsigned} ${keyring}
 
+## Keypairroom
+cat << EOF > keypairroomSalt.json
+{ "salt": "qf3skXnPGFMrE28UJS7S8BdT8g==" }
+EOF
+cat << EOF > keypairroomChallengeInput.json
+{
+  "userChallenges": {
+    "question1": "brontolo",
+    "question2": "pisolo",
+    "question3": "mammolo",
+    "question4": "cucciolo",
+    "question5": "gongolo"
+  },
+  "username": "JohnDoe"
+}
+EOF
+testzen keypairoomClient '{"mnemonic_backup":"goat fantasy apart warfare toilet purse that extend merge robot debris rice what side nephew true awake silk jazz drill mix idle girl focus","secret_seed":"ZApgKXuOPV03+oeLd2Th3J+dj+UXShA5Ed2heODhWJI="}' keypairroomChallengeInput.json keypairroomSalt.json
+
+
+
 # cleanup tempfiles
 clean_vectors
 rm -f $gqlsigned
