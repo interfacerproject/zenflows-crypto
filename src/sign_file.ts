9f980cb86d3c9b090bff411c0ccbf681061c9536
Jaromil <jaromil@dyne.org>
2022-08-31 21:46:33 +0200

    completed verification of hash and file

--- src/sign_file.ts
+++ src/sign_file.ts
@@ -6,11 +6,11 @@ Given I have a 'url64' named 'hashedFile'
 Given I have a 'keyring'
 
 When I create the hash of 'hashedFile'
-and I create the eddsa signature of 'hashedFile'
+and I rename 'hash' to 'signed_hash'
+and I create the eddsa signature of 'signed_hash'
 
 Then print 'eddsa signature'
-# Then print 'hashedFile' as 'base64'
-Then print 'hash' as 'hex'
+and print 'signed_hash' as 'url64'
 `}
 
 export default signFile
