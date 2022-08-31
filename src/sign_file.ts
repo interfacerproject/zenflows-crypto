3beba928e5b6e1a1a87d416bd7d8e62122a90684
Jaromil <jaromil@dyne.org>
2022-08-31 14:42:25 +0200

    fix sign and cover with test

--- src/sign_file.ts
+++ src/sign_file.ts
@@ -5,11 +5,11 @@ Scenario eddsa: sign the hash of a binary file
 Given I have a 'url64' named 'hashedFile'
 Given I have a 'keyring'
 
-When I create the eddsa signature of 'hashedFile'
-And I create the hash of 'hashedFile'
+When I create the hash of 'hashedFile'
+and I create the eddsa signature of 'hashedFile'
 
 Then print 'eddsa signature' as 'base64'
-Then print 'hashedFile' as 'base64'
+# Then print 'hashedFile' as 'base64'
 Then print 'hash' as 'hex'
 `}
 
